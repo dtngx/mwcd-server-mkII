a7a70dd751e71ced0ccabd6ab01599de1e96cfc0
Kevin Schwarz <k.schwarz.arc@gmail.com>
2021-01-29 19:23:07 +0100

    first commit

---
+++ database.js
@@ -0,0 +1,52 @@
+var sqlite3 = require('sqlite3').verbose()
+
+let db = new sqlite3.Database('hk_complete.db', (err) => {
+  if (err) {
+    console.error(err.message);
+    throw err
+   }
+  console.log('Connected to the H+K database.');
+});
+
+// create table 'project'
+var sql='CREATE TABLE project (project_id INT NOT NULL UNIQUE, name TEXT, startdate DATE)';
+db.run(sql, (err) => {
+  if (err) {
+    console.log('Project Table already created.');
+  }else{
+    console.log('Project Table created.');
+  }
+});
+
+// create table 'user'
+sql='CREATE TABLE user (user_id INT NOT NULL UNIQUE, username TEXT, password TEXT, usertype TEXT, email TEXT)';
+db.run(sql, (err) => {
+  if (err) {
+    console.log('User Table already created.');
+    }
+    else {
+      //creating default users
+      console.log('User Table created.');
+      var insert = 'INSERT INTO user (user_id, username, password, usertype) VALUES(?, ?, ?, ?)';
+      db.run(insert, [00001, "manager", "manager", "manager"])
+      db.run(insert, [00002, "client", "client", "client"])
+      db.run(insert, [00003, "employee", "employee", "employee"])
+    }
+});
+
+module.exports = db
+
+
+//TODO 
+
+/* 
+add 
+
+  , projects ARRAY
+
+to the User Table
+*/
+
+/*
+add a KnowledgeTable
+*/
---
+++ server.js
@@ -0,0 +1,126 @@
+var express = require("express")
+var cors = require('cors');
+var bodyParser = require("body-parser");
+var db = require("./database.js")
+
+var app = express()
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8085
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+});
+
+
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.json({ "message": "Server is running on port %PORT%".replace("%PORT%", HTTP_PORT) })
+});
+
+//check login
+app.post("/users/login/", (req, res) => {
+    var errors = []
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+    sql = 'SELECT * FROM user WHERE username = ? AND password = ?';
+    params = [req.body.username, req.body.password]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        return row
+            ? res.json({
+                "message": "success",
+                "data": row.usertype,
+                "user_id": row.user_id
+            })
+            : res.json({
+                "message": "incorrect username/password"
+            })
+
+    });
+});
+
+// Create a new project
+app.post("/projects/", (req, res, next) => {
+    var errors = []
+    var data = {
+        id: req.body.project_id,
+        name: req.body.name,
+        startdate: req.body.startdate
+    }
+    if (!data.name || !data.id || !data.startdate) {
+        errors.push("Project ID, Name or Startdate for project not specified");
+    }
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+
+    sql = 'INSERT INTO project (project_id, name, startdate) VALUES (?, ?, ?)';
+    var params = [data.project_id, data.name, data.startdate]
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data
+        })
+    });
+});
+
+//create new user
+
+//create new knowledge
+
+// Default response
+app.use(function (req, res) {
+    res.status(404);
+});
+
+
+/*
+
+
+// list all projects DANGER
+app.get("/projects", (req, res, next) => {
+    var sql = `SELECT * FROM project`;
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+// list all users DANGER
+app.get("/users", (req, res, next) => {
+    sql = `SELECT * FROM user`;
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+*/
